060850bf1cb832a5f32521691344f68d85b4d5c0
Bruno Garcia <brunoaagarcia636@gmail.com>
2025-06-08 16:48:21 -0300

    criando estrutura da classe clientes

--- CdClientes.cs
+++ CdClientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace p2BRUNO
 {
     class CdClientes
     {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public CdClientes(string nome, string email, string cpf)
+        {
+            Id= Guid.NewGuid();
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+       
+
+        }
+
+
     }
 }
