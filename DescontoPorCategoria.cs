793f7f472a1d42c608ca9744ebb48a7100a742c7
Bruno Garcia <brunoaagarcia636@gmail.com>
2025-06-09 12:30:23 -0300

    implementando estrategia  de desconto por categoria do produto

--- DescontoPorCategoria.cs
+++ DescontoPorCategoria.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace p2BRUNO
 {
-    class DescontoPorCategoria
+    public class DescontoPorCategoria : IDescontoStrategy
     {
+        private readonly string _categoriaAlvo;
+        private readonly decimal _percentual;
+
+        public DescontoPorCategoria(string categoriaAlvo, decimal percentual)
+        {
+            _categoriaAlvo = categoriaAlvo;
+            _percentual = percentual;
+        }
+
+        public decimal CalcularDesconto(ItemPedido item)
+        {
+            if (item.Produto.Categoria == _categoriaAlvo)
+                return item.Subtotal * _percentual;
+
+            return 0;
+        }
     }
 }
