127d7027b3c0ededda0f4f717032b7b5cd40d0e1
Bruno Garcia <brunoaagarcia636@gmail.com>
2025-06-09 03:50:57 -0300

    adicionando PedidoFactory para encapsular criação de pedido

--- PedidoFactory.cs
+++ PedidoFactory.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace p2BRUNO
 {
-    class PedidoFactory
+    public class PedidoFactory
     {
+        public Pedido CriarPedido(Cliente cliente, List<ItemPedido> itens)
+        {
+            if (cliente == null)
+                throw new ArgumentNullException(nameof(cliente));
+            if (itens == null || itens.Count == 0)
+                throw new ArgumentException("coloque um item.");
 
-
-
+            var pedido = new Pedido(cliente, itens);
+            return pedido;
+        }
     }
 }
