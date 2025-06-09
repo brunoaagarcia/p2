1a18c848f3b68dde210d669721ccc1df6e663634
Bruno Garcia <brunoaagarcia636@gmail.com>
2025-06-09 15:05:34 -0300

    implementa AplicacaoDeDescontos com injeção de dependência e Strategy

--- AplicacaoDeDescontos.cs
+++ AplicacaoDeDescontos.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace p2BRUNO
 {
-    class AplicacaoDeDescontos
+    public class AplicacaoDeDescontos
     {
+        private readonly IEnumerable<IDescontoStrategy> _estrategias;
+        private readonly ILogger _logger;
+
+        public AplicacaoDeDescontos(IEnumerable<IDescontoStrategy> estrategias, ILogger logger)
+        {
+            _estrategias = estrategias ?? throw new ArgumentNullException(nameof(estrategias));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public void AplicarDescontos(Pedido pedido)
+        {
+            foreach (var estrategia in _estrategias)
+            {
+                var desconto = estrategia.CalcularDesconto(pedido);
+                if (desconto > 0)
+                {
+                    _logger.Log($"Desconto aplicado: R$ {desconto:F2} ({estrategia.GetType().Name})");
+                }
+            }
+        }
     }
 }
